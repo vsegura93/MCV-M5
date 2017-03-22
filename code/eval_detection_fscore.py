867aa6d3a74c69e01d3eb72d9ba6b8480f349f0a
Víctor Segura Tirado <victor.seguratir@e-campus.uab.cat>
2017-03-22 21:37:10 +0100

    jpdate evaluation detection fscore

--- code/eval_detection_fscore.py
+++ code/eval_detection_fscore.py
@@ -11,8 +11,11 @@ from models.yolo import build_yolo
 from tools.yolo_utils import *
 
 # Input parameters to select the Dataset and the model used
-dataset_name = 'Udacity' #set to TT100K_detection otherwise
-model_name = 'tiny-yolo' #set to yolo otherwise
+#dataset_name = 'Udacity' #set to TT100K_detection otherwise
+#model_name = 'tiny-yolo' #set to yolo otherwise
+dataset_name = 'TT100K_detection'
+model_name = 'tiny-yolo'
+
 
 # Net output post-processing needs two parameters:
 detection_threshold = 0.6 # Min probablity for a prediction to be considered
