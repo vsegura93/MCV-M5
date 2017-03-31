8a5e7c93f7b7c5af3aaa30b5dfdbfe0b8d9e58e2
Víctor Segura Tirado <victor.seguratir@e-campus.uab.cat>
2017-03-31 12:32:53 +0200

    Refactor run_fcn8.sh

--- code/config/camvid_segmentation.py
+++ code/config/camvid_segmentation.py
@@ -14,8 +14,8 @@ weights_file                 = 'weights.hdf5'  # Training weight file name
 
 # Parameters
 train_model                  = True            # Train the model
-test_model                   = False           # Test the model
-pred_model                   = False           # Predict using the model
+test_model                   = True            # Test the model
+pred_model                   = True            # Predict using the model
 
 # Debug
 debug                        = False           # Use only few images for debuging
