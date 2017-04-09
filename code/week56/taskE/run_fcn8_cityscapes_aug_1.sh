1fad8655f4a0013606017c5fc248bb9abb150f51
Víctor Segura Tirado <victor.seguratir@e-campus.uab.cat>
2017-04-09 17:50:53 +0200

    Modified script to execute taskE on week56

--- code/week56/taskE/run_fcn8_cityscapes_aug_1.sh
+++ code/week56/taskE/run_fcn8_cityscapes_aug_1.sh
@@ -1,1 +1,1 @@
-python ../../train.py -c ../../config/cityscapes_segmentation_fcn8.py -e FCN8 --local_path /datatmp/Datasets/segmentation/  > log_fcn8_cityscapes_aug1.txt &
+python ../../train.py -c ../../config/cityscapes_segmentation_fcn8.py -e FCN8_aug --local_path /datatmp/Datasets/segmentation/  > log_fcn8_cityscapes_aug1.txt &
